149d02b75db1c20e4db17e16bfaba1e16b3a75c8
James <jhaymesifiok@gmail.com>
2023-01-06 20:54:57 +0100

    worked on shared preference

--- lib/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/data_sources/number_trivia_remote_data_source.dart
@@ -1,8 +1,27 @@
+
+
 import '../models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class AbstractNumberTriviaRemoteDataSource{
 
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number); 
    Future<NumberTriviaModel> getRandomNumberTrivia(); 
 
+}
+
+
+class NumberTriviaRemoteDataSource implements AbstractNumberTriviaRemoteDataSource{
+  
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) {
+    // TODO: implement getConcreteNumberTrivia
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
 }
